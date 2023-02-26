6ec2ee785ff86952bdd006156536fd89a4c9e15e
hariskhan636 <harisjriv@gmail.com>
2023-02-26 23:26:39 +0500

    final version

---
+++ coe6340c5a5078121cf6edfcf/index.js
@@ -0,0 +1,117 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let pass1El = document.getElementById("pass1-el");
+let pass2El = document.getElementById("pass2-el");
+
+function generate() {
+  //console.log(pass1El)
+  //console.log(pass2El)
+  let pass1 = createPass();
+  let pass2 = createPass();
+
+  pass1El.textContent = pass1;
+  pass2El.textContent = pass2;
+
+  console.log(pass1);
+  console.log(pass2);
+}
+
+function createPass() {
+  let password = "";
+  for (let i = 0; i < 15; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return password;
+}
